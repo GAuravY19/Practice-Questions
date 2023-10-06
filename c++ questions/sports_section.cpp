1678b5e17ed266e7253834848e6b8249f6977f8b
gaurav <gauravy1905@gmail.com>
2023-10-06 05:31:41 +0530

    DAY 007

---
+++ c++ questions/sports_section.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    if (n > 7 && n<=10){
+        cout<<"YES"<<endl;
+    } else{
+        cout<<"NO"<<endl;
+    }
+}
